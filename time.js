290c3f002235680d6a82fe9e3eec5a230d1bd483
krdkrdkrd <143850010+krdkrdkrd@users.noreply.github.com>
2024-07-19 19:54:37 +0300

    time.js

--- time.js
+++ time.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to (19 July 2024 at 8 pm KRT)
-var countDownDate = new Date("19 July, 2024 20:00:00").getTime();
+var countDownDate = new Date("19 July, 2024 20:30").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
